22abd83bfc860fdbd626df495dcc215e598b1966
Moti Asayag <masayag@redhat.com>
2016-02-23 15:02:47 +0200

    Complete missing code pieces for tests to work

--- string_ext.rb
+++ string_ext.rb
@@ -1,2 +1,15 @@
 class StringExt
+
+  def initialize(first, second)
+    @concated = first + second
+    @substracted = first.to_s.sub(second, '')
+  end
+
+  def concat()
+    @concated
+  end
+
+  def substract()
+    @substracted
+  end
 end
